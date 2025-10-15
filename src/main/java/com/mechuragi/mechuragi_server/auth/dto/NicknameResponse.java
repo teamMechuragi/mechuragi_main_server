f8d0b4a7213d98ab83333d3221dd823039e91c74
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-10-16 00:52:37 +0900

    feat: 닉네임 자동생성 api 구현

---
+++ src/main/java/com/mechuragi/mechuragi_server/auth/dto/NicknameResponse.java
@@ -0,0 +1,10 @@
+package com.mechuragi.mechuragi_server.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NicknameResponse {
+    private String nickname;
+}
