26fe0de775aff1d97e6dc00e5b2d753d7d9d8e84
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-10-23 01:15:02 +0900

    chore: 누락된 dev 환경 설정 추가 및 aws config 수정

--- src/main/java/com/mechuragi/mechuragi_server/global/config/AwsConfig.java
+++ src/main/java/com/mechuragi/mechuragi_server/global/config/AwsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,19 +17,34 @@ public class AwsConfig {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key:#{null}}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secret-key:#{null}}")
     private String secretKey;
+
+    /**
+     * AWS 자격 증명 제공자 설정
+     * - local 환경: access-key, secret-key 사용 (StaticCredentialsProvider)
+     * - dev/prod 환경: IAM Role 사용 (DefaultCredentialsProvider)
+     */
+    private AwsCredentialsProvider getCredentialsProvider() {
+        if (accessKey != null && secretKey != null && !accessKey.isEmpty() && !secretKey.isEmpty()) {
+            // local 환경: 명시적 자격 증명 사용
+            return StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKey, secretKey)
+            );
+        } else {
+            // dev/prod 환경: IAM Role 사용 (EC2 Instance Profile 등)
+            return DefaultCredentialsProvider.create();
+        }
+    }
 
     @Bean
     public SesClient sesClient() {
         return SesClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(getCredentialsProvider())
                 .build();
     }
 
@@ -35,9 +52,7 @@ public class AwsConfig {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(getCredentialsProvider())
                 .build();
     }
 }
