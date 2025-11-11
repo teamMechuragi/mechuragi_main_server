d45fffd9c19b5109722208f39561f06f51fbc6e8
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-11-11 14:05:14 +0900

    feat: VoteNotificationMessage DTO 생성

---
+++ src/main/java/com/mechuragi/mechuragi_server/domain/vote/dto/VoteNotificationMessage.java
@@ -0,0 +1,19 @@
+package com.mechuragi.mechuragi_server.domain.vote.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteNotificationMessage {
+    private Long voteId;
+    private String title;
+    private VoteNotificationType type;
+    private LocalDateTime timestamp;
+}
