7168515e07f551d0e918b18f04ad8232cac0658b
KIM HUIJU <140360418+hj1016@users.noreply.github.com>
2026-02-09 12:08:17 +0900

    feat: 이미지 업로드 Pre-signed URL 방식 추가 (#76)
    - S3Presigner Bean 추가
    - generatePresignedUploadUrl() 메서드 추가
    - /presigned-url 엔드포인트 추가
    - 클라이언트가 S3에 직접 업로드하여 속도 개선

--- src/main/java/com/mechuragi/mechuragi_server/global/config/AwsConfig.java
+++ src/main/java/com/mechuragi/mechuragi_server/global/config/AwsConfig.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.ses.SesClient;
 
 @Configuration
@@ -51,6 +52,14 @@ public class AwsConfig {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(getCredentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(getCredentialsProvider())
                 .build();
